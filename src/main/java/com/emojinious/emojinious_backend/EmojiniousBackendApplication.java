b93044e6dd556bb3e8d9b8d7138dc4c80c940127
yhcho0405 <yhcho0405@kakao.com>
2024-07-25 19:55:50 +0900

    first commit

---
+++ src/main/java/com/emojinious/emojinious_backend/EmojiniousBackendApplication.java
@@ -0,0 +1,13 @@
+package com.emojinious.emojinious_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmojiniousBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmojiniousBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emojinious/emojinious_backend/EmojiniousBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emojinious.emojinious_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmojiniousBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
